7284aad73ca2f2bbe382c3fe6129c7abbc788db1
biraj <x.biraj@gmail.com>
2018-02-28 19:39:23 +0530

    jpg to JPG

--- application/views/concept-scheme-b.php
+++
@@ -1,5 +0,0 @@
-        <div class='main career'>
-            
-            <img style='margin-left: 120px;' src='<?php echo $base , $img ?>pages/scheme-b.png'>
-            
-        </div>
--- application/views/contact.php
+++ application/views/contact.php
@@ -1,8 +1,8 @@
             <div class='main contact'>
                 <div class='b-contact'>
 
-                    <img style='margin-top: 50px; margin-left: 120px;' src='<?php echo $base , $img ?>pages/contact-info/contact.jpg'>
-                    <img style='margin-left: 120px;' src='<?php echo $base , $img ?>pages/contact-info/info.jpg'>
+                    <img style='margin-top: 50px; margin-left: 120px;' src='<?php echo $base , $img ?>pages/contact-info/contact.JPG'>
+                    <img style='margin-left: 120px;' src='<?php echo $base , $img ?>pages/contact-info/info.JPG'>
 
                 </div>
             </div>
