3e64102ca56cf1310193272c39ff813ff430286a
biraj <x.biraj@gmail.com>
2018-02-28 19:43:37 +0530

    img rename

--- application/views/concept.php
+++ application/views/concept.php
@@ -1,5 +1,5 @@
         <div class='main career'>
-            <img style='margin-top: 50px; margin-left: 80px; margin-bottom: 50px;' src='<?php echo $base , $img ?>pages/concept/career-plan.JPG'>
-            <img style='margin-left: 80px;' src='<?php echo $base , $img ?>pages/concept/rewards-awards.JPG'>
+            <img style='margin-top: 50px; margin-left: 80px; margin-bottom: 50px;' src='<?php echo $base , $img ?>pages/concept/career-plan_.JPG'>
+            <img style='margin-left: 80px;' src='<?php echo $base , $img ?>pages/concept/rewards-awards_.JPG'>
 
         </div>
--- application/views/contact.php
+++ application/views/contact.php
@@ -1,8 +1,8 @@
             <div class='main contact'>
                 <div class='b-contact'>
 
-                    <img style='margin-top: 50px; margin-left: 120px;' src='<?php echo $base , $img ?>pages/contact-info/contact.JPG'>
-                    <img style='margin-left: 120px;' src='<?php echo $base , $img ?>pages/contact-info/info.JPG'>
+                    <img style='margin-top: 50px; margin-left: 120px;' src='<?php echo $base , $img ?>pages/contact-info/contact_.JPG'>
+                    <img style='margin-left: 120px;' src='<?php echo $base , $img ?>pages/contact-info/info_.JPG'>
 
                 </div>
             </div>
